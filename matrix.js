ce90232fd8f2e9623fd9d12297b13935bad98060
Rafał Hirsz <rafal@hirsz.co>
2021-05-13 18:40:44 +0200

    Cache Matrix GET requests

--- matrix.js
+++ matrix.js
@@ -1,11 +1,31 @@
 const { MatrixClient } = require("matrix-blog");
 const fetch = require("node-fetch");
+const Cache = require("@11ty/eleventy-cache-assets");
+
+function cachingFetch(url, options) {
+  if (!options) options = {};
+
+  // Do not cache other requests than GET.
+  if (options.method && options.method.toLowerCase() !== "get") {
+    return fetch(url, options);
+  }
+
+  return Cache(url, {
+    duration: "1h",
+    fetchOptions: options,
+  }).then((buffer) => ({
+    ok: true,
+    status: 200,
+    json: () => Promise.resolve(JSON.parse(buffer.toString("utf8"))),
+    text: () => Promise.resolve(buffer.toString("utf8")),
+  }));
+}
 
 function getMatrixClient() {
   const client = new MatrixClient(
     process.env.MATRIX_SERVER_NAME,
     process.env.MATRIX_HOMESERVER_URL,
-    fetch
+    cachingFetch
   );
   client.setAccessToken(process.env.MATRIX_ACCESS_TOKEN);
   return client;
